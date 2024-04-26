c738fa547bda9f0d5be7ece07ff5768e271d16f3
unknown <jployola@pucp.edu.pe>
2024-04-25 19:26:14 -0500

    primer commit

---
+++ src/main/java/org/example/gticslab420180941/GticsLab420180941Application.java
@@ -0,0 +1,13 @@
+package org.example.gticslab420180941;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab420180941Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab420180941Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gticslab420180941/GticsLab420180941ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gticslab420180941;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab420180941ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
