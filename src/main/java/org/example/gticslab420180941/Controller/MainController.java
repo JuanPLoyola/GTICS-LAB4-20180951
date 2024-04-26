b921b2253af366106830f4dc3d064b264db3c556
unknown <jployola@pucp.edu.pe>
2024-04-25 20:40:29 -0500

    a1

--- src/main/java/org/example/gticslab420180941/Controller/MainController.java
+++ src/main/java/org/example/gticslab420180941/Controller/MainController.java
@@ -1,11 +1,15 @@
 package org.example.gticslab420180941.Controller;
 
+import org.example.gticslab420180941.Entity.Employees;
 import org.example.gticslab420180941.Reposiroty.EmployeeReposiroty;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLOutput;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -20,9 +24,17 @@ public class MainController {
     public String Empleados(){
         return "Empleados";
     }
+
+    @GetMapping("/listar")
+    public String listarEmpleados(){
+        List<Employees> lista = employeeReposiroty.findAll():
+        for(Employees e : lista){
+            System.out.print(e.getEmployee_id());
+            System.out.print(e.getFirts_name());
+            System.out.print(e.getLast_name());
+
+        }
 }
 
-@GetMapping("/listar")
-public String listarEmpleados(){
 
 }
