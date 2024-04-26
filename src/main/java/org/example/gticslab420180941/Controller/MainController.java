e6ef5388e12be09a30263518479dde9671ea12a0
unknown <jployola@pucp.edu.pe>
2024-04-25 20:29:16 -0500

    a

--- src/main/java/org/example/gticslab420180941/Controller/MainController.java
+++ src/main/java/org/example/gticslab420180941/Controller/MainController.java
@@ -1,6 +1,7 @@
 package org.example.gticslab420180941.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,4 +13,9 @@ public class MainController {
     public String Empleados(){
         return "Empleados";
     }
+}
+
+@GetMapping("/listar")
+public String listarEmpleados(){
+
 }
